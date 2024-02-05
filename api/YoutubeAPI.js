d82fc294e672bb096813003c356efaef9db4f8c1
Jerry Duran <gduranjerry@gmail.com>
2024-02-04 21:02:11 -0800

    API

---
+++ api/YoutubeAPI.js
@@ -0,0 +1,17 @@
+import env from '../config.json' assert { type: "json" };
+const baseURL = `https://www.googleapis.com/youtube/v3`;
+//@ts-ignore
+export const getSearch = async ({ query, order }) => {
+    const params = {
+        key: env.YOUTUBE_API_KEY,
+        type: 'video',
+        part: 'snippet',
+        maxResults: '20',
+        ...(order && { order }),
+        query,
+    };
+    let paramsString = new URLSearchParams(params).toString();
+    const response = await fetch(`${baseURL}/search?${paramsString}`);
+    return await response.json();
+};
+//# sourceMappingURL=YoutubeAPI.js.map
